249e56094e10420fd33b33b7314867d0c1abde78
Aarow Epstein <aarowepstein1@gmail.com>
2022-12-23 18:31:32 -0700

    commit tree a4315286251e14565a1f6ca368f1caf965172325

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    const firstTwoDrivers = drivers.slice(0,2);
+    return firstTwoDrivers;
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    const lastTwoDrivers = drivers.slice(-2);
+    return lastTwoDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (multiplier) {
+    function fareMultiplier (fare) {
+        return fare * multiplier;
+    }
+    return fareMultiplier;
+}
+
+const fareDoubler = createFareMultiplier(2);
+//fareMultiplier (fare) {return fare * 2}
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func) {
+    
+    if (func === returnFirstTwoDrivers){
+       const blam = selectingDrivers[0];
+       return blam(drivers); 
+    } else {
+        const lastDrivers = selectingDrivers[1];
+        return lastDrivers(drivers);
+    }
+}
